33cc196d08fc5c9842765c05f1a772e9f30422a5
abed <abdullah.almussawi@gmail.com>
2024-04-20 15:56:36 -0400

    adding the CreatDB class

---
+++ src/main/java/com/abedmussawi/www/Main.java
@@ -0,0 +1,9 @@
+package com.abedmussawi.www;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main{
+    public static void main(String[] args) throws SQLException {
+        CreatDB db = new CreatDB();
+    }
+}
